593021346ea50367362b9b0d8cae00774ccf0ad3
allison cortez <allizetorc@gmail.com>
2019-11-07 21:38:44 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,17 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @receiver = receiver
+    @sender = sender 
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid? 
+    # both_accounts = sender + receiver
+    
+    BankAccount.status == "open" && BankAccount.balance > 0
+  end
+  
 end
