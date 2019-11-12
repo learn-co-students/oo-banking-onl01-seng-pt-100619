41861dddb9b461314cdc43703902862098a6445f
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-12 08:45:19 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class BankAccount
+  attr_reader :name,:balance, :status
+
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+    #binding.pry
+
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    #@broke.balance = 0
+    #@status = 
+  end
+
+  def close_account
+    @status = "closed"
+    #binding.pry
+  end
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, status)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+  end
+
+  def valid?
+  end
+
 end
