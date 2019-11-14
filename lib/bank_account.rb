73c0a2539fdcc79146850f18bc0f42723c376c46
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-11-14 05:50:10 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class BankAccount
+  attr_reader :account, :name
+  attr_accessor :balance, :status
+  
+  def initialize(name)
+    @account = account
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(balance)
+    @balance += balance
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    @balance > 0 && @status == "open" ? true : false
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 
 end
